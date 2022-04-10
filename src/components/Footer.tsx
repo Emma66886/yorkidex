946ddafddaac6774a335d8e28397858d71550267
Emma66886 <Emma66886@gmail.com>
2022-04-10 02:13:55 +0100

    comit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Layout, Row, Col, Grid } from 'antd';
+import Link from './Link';
+import { helpUrls } from './HelpUrls';
+import { useReferrer } from '../utils/referrer';
+const { Footer } = Layout;
+const { useBreakpoint } = Grid;
+
+const footerElements = [
+  // {
+  //   description: 'NEW BOLE TOKEN',
+  //   link: helpUrls.newboletoken,
+  // },
+  { description: 'YORKI WEBSITE', link: helpUrls.yorktokenhome },
+  { description: 'DISCORD', link: helpUrls.discord },
+  { description: 'TELEGRAM', link: helpUrls.telegram },
+  { description: 'TWITTER', link: helpUrls.twitter },
+  { description: 'BUY YORKIPOOCOIN', link: helpUrls.buyyork },
+];
+
+export const CustomFooter = () => {
+  const smallScreen = !useBreakpoint().lg;
+  const { refCode, allowRefLink } = useReferrer();
+  return (
+    <Footer
+      style={{
+        height: '55px',
+        paddingBottom: 10,
+        paddingTop: 10,
+      }}
+    >
+      {refCode && allowRefLink && (
+        <Row justify="center">Your referrer is {refCode}</Row>
+      )}
+      <Row align="middle" gutter={[16, 4]}>
+        {!smallScreen && (
+          <>
+            <Col flex="auto" />
+            {footerElements.map((elem, index) => {
+              return (
+                <Col key={index + ''}>
+                  <Link external to={elem.link}>
+                    {elem.description}
+                  </Link>
+                </Col>
+              );
+            })}
+          </>
+        )}
+        <Col flex="auto">{/*  <DexProgramSelector />*/}</Col>
+      </Row>
+    </Footer>
+  );
+};
---
+++ src/components/HelpUrls.ts
@@ -0,0 +1,10 @@
+export const helpUrls = {
+  customerSupport: 'https://t.me/ProjectSerum',
+  customerSupportZh: 'https://t.me/ProjectSerum_Chinese',
+  contactEmail: 'mailto:contact@projectserum.com',
+  discord: 'https://discord.gg/HDsSFJcS',
+  telegram: 'https://t.me/yorkipoocoin',
+  yorktokenhome: 'https://yorkipoocoin.com',
+  twitter: 'https://twitter.com/yorkipoocoin',
+  buyyork: 'http://dex.yorkipoocoin.com/market/2yAM8Vnpv3rNtN5TDunH8r5QwG9sZhQsGKcEdpRi245n',
+};
